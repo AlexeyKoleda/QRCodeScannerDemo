adca41f396ca051d8e1efce5daf42eee9b8f2ea3
Alexey Koleda <alexeykoleda@icloud.com>
2022-10-14 13:08:19 +0300

    Implement createCaptureSession function

--- QRCodeScannerDemo/Scanner.swift
+++ QRCodeScannerDemo/Scanner.swift
@@ -5,12 +5,42 @@
 //  Created by Alexey Koleda on 14.10.2022.
 //
 
-import Foundation
+import UIKit
 import AVFoundation
-
 
 class Scanner: NSObject {
     private var viewController: UIViewController
     private var captureSession: AVCaptureSession?
     private var codeOutputandler: (_ code: String) -> Void
+    
+    private func createCaptureSession() -> AVCaptureSession? {
+        do {
+            let captureSession = AVCaptureSession()
+            
+            guard let captureDevice = AVCaptureDevice.default(for: .video)
+            else { return nil }
+            
+            // Add device input
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            if captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            }
+            
+            // Add metadata output
+            let metaDataOutput = AVCaptureMetadataOutput()
+            if captureSession.canAddOutput(metaDataOutput) {
+                captureSession.addOutput(metaDataOutput)
+                
+                if let vc = self.viewController as? AVCaptureMetadataOutputObjectsDelegate {
+                    metaDataOutput.setMetadataObjectsDelegate(vc, queue: DispatchQueue.main)
+                    metaDataOutput.metadataObjectTypes = self.metaObjectTypes()
+                } else {
+                    return nil
+                }
+            }
+        } catch {
+            return nil
+        }
+        return captureSession
+    }
 }
